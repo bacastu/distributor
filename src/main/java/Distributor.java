db9e1848bff3d33e0ca0ca10c3364d7d76d5b616
bacastu <baptiste.calin@student.umons.ac.be>
2018-03-05 19:23:31 +0100

    Log4J

--- src/main/java/Distributor.java
+++ src/main/java/Distributor.java
@@ -1,8 +1,18 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Created by bacalin on 03-03-18.
  */
 public class Distributor {
+
+    private static Logger log = LogManager.getLogger(Distributor.class);
+
     public static void main(String[] arg) {
         System.out.println("Start Maven project");
+
+        if(log.isDebugEnabled()){
+            log.debug("Start Maven project with log4J");
+        }
     }
 }
